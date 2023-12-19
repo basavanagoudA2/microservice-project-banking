6cf59d716926884a4a8d2c1951b10781b6a0160e
Basavanagoud A <bsgoud78900@gmail.com>
2023-12-19 19:57:07 +0530

    added banking-core-service-registery and banking-gateway project

---
+++ banking-core-service-registry/banking-core-service-registry/src/main/java/com/javatodev/finance/InternetBankingServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.javatodev.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class InternetBankingServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingServiceRegistryApplication.class, args);
+	}
+
+}
